7ad47a718a486fd74c6645b33f9ec68cba949b71
Abul Farah Ansari <dwe.golden@gmail.com>
2026-07-24 02:10:13 +0530

    Fix Vercel routing

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://lottery-backend2.onrender.com/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
 export default api;
