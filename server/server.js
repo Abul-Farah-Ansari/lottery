c206986dcff051a9302460fd281b270ab82b5716
Abul Farah Ansari <dwe.golden@gmail.com>
2026-07-25 16:59:21 +0530

    Switch to Brevo Email API

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
+      // "http://localhost:5173",
       "https://lottery-5wjp.vercel.app",
     ],
     credentials: true,
