ab5187f7b40c6a313dedf345c09b853fef3890ba
Abul Farah Ansari <dwe.golden@gmail.com>
2026-07-30 14:51:37 +0530

    updated url

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -5,8 +5,8 @@ import LiveResult from "./LiveResult";
 import characterImage from "../assets/images/character (1).png";
 import prizeImage from "../assets/images/prize.png";
 
-import leftCard from "../assets/images/1.png";
-import rightCard from "../assets/images/2.png";
+import leftCard from "../assets/images/1.jpg";
+import rightCard from "../assets/images/2.jpg";
 
 function Hero() {
   
